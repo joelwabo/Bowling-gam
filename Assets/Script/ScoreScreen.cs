fd3f847f47fe9fb797a617750b190e6e1cf0fde2
Joel WABO <joelwabo@yahoo.fr>
2016-12-22 04:31:32 +0100

    screenScore script

--- Assets/Script/ScoreScreen.cs
+++ Assets/Script/ScoreScreen.cs
@@ -9,44 +9,89 @@ public class ScoreScreen : MonoBehaviour {
 	public int nbrePlayer;
 	public string name;
 	private int total;
-	private TextMesh[] pointText;
-	private TextMesh[] totalText;
+	int nbreStrike = 0;
+	int nbreSpare = 0;
+	private List<TextMesh> pointText;
+	private List<TextMesh> totalText;
 	private TextMesh frameTitle;
 	private TextMesh scoreText;
+	private TextMesh nameText;
 
 	// Use this for initialization 
 	void Start () {
-		TextMesh nameText = GameObject.Find("NameText").GetComponent<TextMesh>();
+		nameText = GameObject.Find("NameText").GetComponent<TextMesh>();
 		frameTitle = GameObject.Find("FrameTitle").GetComponent<TextMesh>();
 		scoreText = GameObject.Find("ScoreText").GetComponent<TextMesh>();
-		pointText = GameObject.Find("Point").GetComponents<TextMesh>();
-		totalText = GameObject.Find("Total").GetComponents<TextMesh>();
-
+		pointText = getTextMesh("point");
+		totalText = getTextMesh("total");
 		nameText.text = name;
+		List<int[]> list = new List<int[]> ();
+		int[] tab = new int[2];
+		for (int i = 0; i < 11; i++) {
+			tab [0] = 2;
+			tab [1] = 3;
+			if (i == 0) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 1) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 2) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 3) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 4) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 5) {
+				tab [0] = 10;
+				tab [1] = 0;
+			}
+			if (i == 6) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 7) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 8) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 9) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			if (i == 10) {
+				tab [0] = 0;
+				tab [1] = 10;
+			}
+			list.Add (tab);
+			scoreToShow (list);
+		}
 
 		/*pointText = GameObject.Find("point").GetComponents<TextMesh>();
-		totalText = GameObject.Find("total").GetComponents<TextMesh>();
+		totalText = GameObject.Find("total").GetComponents<TextMesh>();*/
 
 		//GetComponentsInchildren
-		/*TextMesh[] texts = gameObject.GetComponentsInChildren<TextMesh>();
-		TextMesh[] point = new TextMesh[21];
-		TextMesh[] pointTotal = new TextMesh[10];
-		int i = 0, j = 0; k = 0;
-		foreach (var text in texts) {
-			if(texts.name.Contains("point")){
-				point[i] = text;
-				i++;
-			}
-			if(texts.name.Contains("total")){
-				pointTotal[j] = text;
-				j++;
-			}
-			if(texts.name.Contains("somme")){
-				totalText = text;
-			}
-				
-		}*/
 
+	}
+	public List<TextMesh> getTextMesh (string name)
+	{
+		TextMesh[] texts = gameObject.GetComponentsInChildren<TextMesh>();
+		List<TextMesh> textMesh = new List<TextMesh>();
+		foreach (var text in texts) 
+			if(text.name.Contains(name))
+				textMesh.Add(text);
+		return textMesh;
 	}
 
 	public void setScoreText (int count, int round)
@@ -57,50 +102,59 @@ public class ScoreScreen : MonoBehaviour {
 	// on effectue le cacul pour le score suivant les normes du bowling
 	public void scoreToShow(List<int[]> list)
 	{
-		int indice = list.Capacity - 1;
+		int indice = list.Count - 1;
 		int score = 0;
-		int nbreStrike = 0;
 		if (isStrike (list[indice])) {
+			pointText[2*indice].text = " ";
+			if(2*indice+1 < 21)
+				pointText[2*indice+1].text = "X";
 			nbreStrike++;
-			pointText[2*indice].text = "X";
 		} else if (isSpare (list[indice])) {
+			pointText[2*indice].text = list[indice][0].ToString();
 			pointText[2*indice+1].text = "/";
-		} else {
-			if ((indice > 1) && (indice < 10)){
-				if (isSpare (list[indice - 1])) {
+			nbreSpare++;
+		} else {	
+			if ((indice >= 1) && (indice <= 10)){
+				if (nbreSpare>0) {
 					score = 10 + list[indice][0];
 					totalText[indice-1].text = score.ToString();
-				} else if (nbreStrike != 0) {
+					nbreSpare = 0;
+				} 
+				if (nbreStrike>0) {
 					List<int> scoreStrike = strikeScore (list, nbreStrike);
-					for (int i = scoreStrike.Capacity; i > 0; i--)
+					for (int i = 0 ; i < scoreStrike.Count ; i++)
 					{
-						totalText[indice-i].text = scoreStrike[i].ToString();
+						totalText[indice-i-1].text = scoreStrike[i].ToString();
 					}
 					nbreStrike = 0;
 				}
 			}
-			if (indice == 10)
-				score = list[indice - 1][0] + list[indice - 1][1] + list[indice - 1][0];		
-			else
-				score = list[indice][0] + list[indice][1];
-			setTotal (indice);
-			totalText[indice].text = score.ToString();
+			if (indice == 10) {
+				score = list [indice - 1] [0] + list [indice - 1] [1] + list [indice] [0];	
+				pointText[2*indice].text = list[indice][0].ToString();
+				totalText[indice - 1].text = score.ToString();
+			} else {
+				score = list [indice] [0] + list [indice] [1];
+				pointText[2*indice].text = list[indice][0].ToString();
+				pointText[2*indice+1].text = list[indice][1].ToString();
+				totalText[indice].text = score.ToString();
+			}
+			setTotal ();
 		}
-
 	}
 
-	public void setTotal(int indice)
+	public void setTotal()
 	{
 		int total = 0;
-		foreach (var text in totalText) {
-			total = total + int.Parse(text.text);
+			for (int i = 0; i < totalText.Count; i++){
+				total = total + int.Parse(totalText[i].text);
 		}
 		scoreText.text = total.ToString();
 	}
 
 	public List<int> strikeScore(List<int[]> list, int nbreStrike)
 	{
-		int indice = list.Capacity - 1;
+		int indice = list.Count - 1;
 		List<int> scoreStrike = new List<int>();
 		if (indice == 10) {
 			for (int i = 0; i < nbreStrike; i++) {
@@ -111,6 +165,7 @@ public class ScoreScreen : MonoBehaviour {
 				else if (i > 1)
 					scoreStrike.Add (30);
 			}
+			Debug.Log ("ICI");
 		} else {
 			for (int i = 0; i < nbreStrike; i++) {
 				if (i == 0)
@@ -133,7 +188,7 @@ public class ScoreScreen : MonoBehaviour {
 	}
 	public bool isSpare(int[] tab)
 	{
-		if (tab [0] + tab [1] == 10)
+		if ((tab[0] + tab[1]) == 10)
 			return true;
 		else
 			return false;
